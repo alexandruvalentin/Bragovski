59aa3bd9ece139a674102edf29afc4e04edb3b11
alexandruvalentin <alexandru22valentin@gmail.com>
2021-09-13 11:36:14 +0000

    added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
