1a29184b7a6845c79883d9cc0d6220c256938cae
alexandruvalentin <alexandru22valentin@gmail.com>
2021-10-27 01:51:43 +0000

    update testimonials models rate input with min and max value validators

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -13,7 +14,9 @@ class Testimonials(models.Model):
                              related_name='testimonials')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     comment = models.TextField(max_length=543)
-    rate = models.IntegerField(default=0)
+    rate = models.IntegerField(default=0,
+                               validators=[MinValueValidator(0),
+                                           MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
